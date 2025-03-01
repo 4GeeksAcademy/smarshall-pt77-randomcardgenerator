24e1600e98b1810ad7b5c5cb116241f9af98bf1c
marlaw21 <marlaw43@comcast.net>
2025-03-01 00:30:36 +0000

    finished product

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+    let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+    let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+    let randomSuitNumber = Math.floor(Math.random() * suit.length);
+    let selectedSuit = suit[randomSuitNumber];
+    
+    document.getElementById('cardContent').innerHTML = cardNumber[randomCardNumber];
+    document.getElementById('aCard').className = "";
+    document.getElementById('aCard').classList.add("card");
+    document.getElementById('aCard').classList.add(getSuiteClass(selectedSuit));
+  }
+  
+  function getSuiteClass(suit) {
+    switch (suit) {
+      case "Diamonds": return "suit-diamonds";
+      case "Spades": return "suit-spades";
+      case "Hearts": return "suit-hearts";
+      case "Clubs": return "suit-clubs";
+    }
+  }
